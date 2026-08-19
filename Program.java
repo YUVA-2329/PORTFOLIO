7a2148e5d2f37c6f56654bbcaaaacd3c7d25c504
YUVA-2329 <yuvakishore.vps@gmail.com>
2026-08-19 11:28:32 +0530

    first

---
+++ Program.java
@@ -0,0 +1,10 @@
+class  Program{
+public static void main(String args[]){
+    int a=10;
+    int b=10;
+ System.out.println("The sum of a and b is: " + (a+b)); 
+ 
+}
+
+
+}
---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main(){
+    int a=20;
+    int b=10;
+    int c=a+b;
+    printf("SUM OF TWO NUMBERS IS %d",c);
+
+}
